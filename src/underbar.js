30cd45cb6223c80c12258951c53b1baffa328581
Jisoo Yoon <jyoon006@gmail.com>
2015-11-25 19:35:00 -0500

    updated and passed test for _.difference function

--- src/underbar.js
+++ src/underbar.js
@@ -440,6 +440,12 @@
   // Take the difference between one array and a number of other arrays.
   // Only the elements present in just the first array will remain.
   _.difference = function(array) {
+	var args = Array.prototype.slice.call(arguments, 1);
+	return _.reduce(args, function(a, b) {
+	 return _.filter(a, function(value) {
+	  return _.indexOf(b, value) === - 1;
+	 });
+	}, array);
   };
 
   // Returns a function, that, when invoked, will only be triggered at most once
